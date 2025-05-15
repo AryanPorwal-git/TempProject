b605f554dbdbad8b2bdb6ab0c1017af3c65e9d8b
Pranav Srinivasa <pranavsrinivas6@gmail.com>
2025-05-15 17:53:27 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import SignOut from './pages/SignOut';
+import SignOut from './Pages/SignOut'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
