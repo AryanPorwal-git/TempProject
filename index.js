162b87768f3ddc1a6f98fa842968c7d2930fe96d
Pranav Srinivasa <pranavsrinivas6@gmail.com>
2025-05-15 17:39:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import SignOut from './pages/SignOut';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
